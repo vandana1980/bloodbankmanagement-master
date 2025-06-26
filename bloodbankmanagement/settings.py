86babb80f1f4aeb080b038f7155d9b9ada1455c0
vandana1980 <vanchaurasia1980@gmail.com>
2025-06-26 13:52:35 +0530

    settings.py

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -141,6 +141,6 @@ EMAIL_HOST_PASSWORD = 'xyz' # host email password required
 # otherwise you will get SMTPAuthenticationError at /contactus
 # this process is required because google blocks apps authentication by default
 EMAIL_RECEIVING_USER = ['to@gmail.com'] # email on which you will receive messages sent from website
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'  # Use BigAutoField for default primary keys
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'  # Use BigAutoField for default primary keys
 
 
